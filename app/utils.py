da17e7183deda64c59313e61e2a68cc94486df5d
lautarodapin <lautarodapin@gmail.com>
2021-05-08 17:35:57 -0300

    match types correction

--- app/utils.py
+++ app/utils.py
@@ -3,9 +3,9 @@ from typing import Dict, Union
 
 
 MATCH_TYPES : Dict[str, Union[bool, int, str]] = {
-    "get_bool": bool,
-    "get_real": float,
-    "get_int": int,
-    "get_dword": int,
-    "get_string": str
+    "get_bool": "bool",
+    "get_real": "float",
+    "get_int": "int",
+    "get_dword": "int",
+    "get_string": "str",
 }
