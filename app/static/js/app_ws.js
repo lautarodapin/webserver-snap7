552a61f26aaf218ae9d93e9e616b4a8593547b78
lautarodapin <lautarodapin@gmail.com>
2021-05-13 22:12:46 -0300

    IndexedDB basic use

--- app/static/js/app_ws.js
+++ app/static/js/app_ws.js
@@ -2,10 +2,15 @@ const instance = axios.create({
     baseURL: `${window.location.protocol}//${window.location.host}/api/`,
     timeout: 10000,
 }); 
+const DB_NAME = "testDb"
+const DB_VERSION = 2
+let DB;
+
 const app = Vue.createApp({
     delimiters: ["[[", "]]"],
     data(){
         return{
+            db: null,
             ws: null,
             loading: false,
             progress: 0,
@@ -14,7 +19,7 @@ const app = Vue.createApp({
             plcs: [],
             areas: [],
             filas: [],
-            datosProcesados: [],
+            // datosProcesados: [],
             data:[{
                 x: [1,2,3,4],
                 y: [10,15,13,17],
@@ -33,10 +38,13 @@ const app = Vue.createApp({
                 return this.searchFila.toLowerCase().split(' ').every(v => `${fila.area} ${fila.string}`.toLowerCase().includes(v))
             })
         },
+        async datosProcesados(){
+            return await this.getDatos();
+        },
     },
     methods:{
-        fetchData(){
-            localStorage.removeItem("datosProcesados")
+        async fetchData(){
+            await this.clearDatos();
             this.ws.send(JSON.stringify({
                 action: "list",
                 request_id: new Date().getTime(),
@@ -46,17 +54,18 @@ const app = Vue.createApp({
                 },
             }))
         },
-        renderDatosProcesados(){
+        async renderDatosProcesados(){
             // mountApp.datosProcesados.map(dato => dato.fila).filter((value, index, self) => self.indexOf(value) === index)
             // console.log(this.datosProcesados)
-            this.datosProcesados = JSON.parse(localStorage.getItem("datosProcesados"))
-            const filas = this.datosProcesados
+            // this.datosProcesados = JSON.parse(localStorage.getItem("datosProcesados"))
+            datosProcesados = await this.datosProcesados
+            const filas = datosProcesados
                 .map(dato => dato.fila)
                 .filter((value, index, self) => self.indexOf(value) === index)
             // console.log(filas)
             var data = []
             for(var i = 0; i < filas.length; i++){
-                var datosFiltrados = this.datosProcesados.filter(dato => dato.fila === filas[i])
+                var datosFiltrados = datosProcesados.filter(dato => dato.fila === filas[i])
                 // console.log(datosFiltrados)
                 var trace = {
                     x: Array.from(datosFiltrados, dato => dato.date),//.split("T").join(" ").substring(0, dato.date.length - 1)), // "2020-01-01T00:02:00Z".split("T").join(" ").substring(0, "2020-01-01T00:02:00Z".length-1)
@@ -83,30 +92,6 @@ const app = Vue.createApp({
             this.data = data;
             this.layout = layout;
         },
-        getPlcs(){
-            return new Promise((resolve, reject) => {
-                instance.get("plcs/")
-                .then(response => {
-                    this.plcs = response.data.results
-                    resolve(response)
-                })
-                .catch(error => {
-                    reject(error.response)
-                })
-            })
-        },
-        getAreas(){
-            return new Promise((resolve, reject) => {
-                instance.get("areas/")
-                .then(response => {
-                    this.areas = response.data.results
-                    resolve(response)
-                })
-                .catch(error => {
-                    reject(error.response)
-                })
-            })
-        },
         getFilas(){
             return new Promise((resolve, reject) => {
                 instance.get("filas/?limit=100")
@@ -119,35 +104,95 @@ const app = Vue.createApp({
                 })
             })
         },
-        addPoint(){
-            this.data[0].x.push(Math.random()); 
-            this.data[0].y.push(Math.random()); 
-            Plotly.redraw(this.$refs.plotlyEl)
-        },
         createWs(){
             var self = this;
             this.ws = new WebSocket("ws://localhost:8000/ws/dato-procesado/")
             this.ws.onopen = function(){
 
             }
-            this.ws.onmessage = function(e){
+            this.ws.onmessage = async function(e){
                 const response = JSON.parse(e.data)
-                console.log(response)
-                data = JSON.parse(localStorage.getItem("datosProcesados"))
-                if (data == null) data = [];
-                data.push(...response.data.results)
-                localStorage.setItem("datosProcesados", JSON.stringify(data))
+                // console.log(response)
+                // for (i=0; i<response.data.results.length; i++){
+                //     const datos = response.data.results[i]
+                //     let data = await self.saveDato(datos)
+                // }
+                await self.saveDatos(response.data.results)
+                let datos = await self.getDatos();
                 if (response.data.offset + response.data.results.length == response.data.count) 
                     return self.renderDatosProcesados();
+                // console.log(datos)
                 // self.datosProcesados.push(...response.data.results)
             }
-        }
+        },
+        async getDb() {
+            return new Promise((resolve, reject) => {
+                if (DB) return resolve(DB);
+                // console.log("OPENING DB", DB);
+                let request = window.indexedDB.open(DB_NAME, DB_VERSION);
+                request.onerror = e => {
+                    // console.log('Error opening db', e);
+                    reject('Error');
+                };
+                request.onsuccess = e => {
+                    DB = e.target.result;
+                    resolve(DB);
+                };
+                request.onupgradeneeded = e => {
+                    // console.log('onupgradeneeded');
+                    let db = e.target.result;
+                    let objectStore = db.createObjectStore("datosProcesados", { autoIncrement: true, keyPath:'id' });
+                };
+            });
+        },
+        async getDatos(){
+            let db = await this.getDb();
+            return new Promise((resolve, reject) => {
+                let transaction = db.transaction(["datosProcesados"], "readonly")
+                transaction.oncomplete = () => resolve(datos)
+                let store = transaction.objectStore("datosProcesados")
+                let datos = []
+                store.openCursor().onsuccess = e => {
+                    let cursor = e.target.result;
+                    if (cursor){
+                        datos.push(cursor.value)
+                        cursor.continue();
+                    }
+                }
+            })
+        },
+        async saveDatos(datos){
+            datos.forEach(dato =>  this.saveDato(dato))//.then(response => console.log("Saved dato", dato)))
+        },
+        async saveDato(dato){
+            let db = await this.getDb();
+            return new Promise((resolve, reject) => {
+                let transaction = db.transaction(["datosProcesados"], "readwrite");
+                transaction.oncomplete = () => {
+                    resolve(dato)
+                };
+                let store = transaction.objectStore("datosProcesados")
+                store.put(dato);
+            })
+        },
+        async clearDatos(){
+            let db = await this.getDb();
+            return new Promise((resolve, reject) => {
+                let transaction = db.transaction(["datosProcesados"], "readwrite")
+                transaction.oncomplete = () => resolve();
+                let objectStore = transaction.objectStore("datosProcesados")
+                let request = objectStore.clear();
+                request.onsuccess = (e) => resolve(e)
+            })
+        },
     },
-    created(){
+    async created(){
         Promise.all([
             this.getFilas(),
         ])
         this.createWs();
+        await this.getDb();
+
         
     },
     mounted(){
